8aa60e0c7a0be0d0c54c86bfe6372e5aca7107f7
icePike0x1 <87804682+icePike0x1@users.noreply.github.com>
2024-04-17 12:24:45 +0300

    2nd commit, improved app.py. added exception checks and deleted methods

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ next_id = ''
 
 app = Flask(__name__)
 
+def chk_post_number(number):
+    try:
+        data=int(number)
+    except:
+        return False
+    return True
+
 def compute_next_id():
     global next_id
     next_id = None 
@@ -36,22 +43,32 @@ def compute_hash(task_id):
     return task_hash
 
 @app.route('/tasks', methods=['GET'])
-def list_tasks():
-    if request.method == 'GET':
+@app.route('/tasks/<post_id>', methods=['GET'])
+def list_tasks(post_id=None):
+    if post_id:
+        if chk_post_number(post_id) and post_id in taskDatabase:
+            return taskDatabase[post_id]
+        else:
+            return f"you have to provide correct post ID (integer number)"
+        
+    else:
         return jsonify(taskDatabase)
-    
-@app.route('/tasks', methods=['DELETE'])
-def delete_task():
-    if request.method == 'DELETE':
-        data = request.get_json()
-        if data:
-            print(type(data))
-            print(data)        
-            task_id = data['id']
-            task_hash = compute_hash(task_id)    
-            del id_to_uuid[task_hash]
-            del taskDatabase[task_id]        
-            return f"task with ID {task_id} was deleted"
+
+@app.route('/tasks/', methods=['DELETE'])
+@app.route('/tasks/<post_id>', methods=['DELETE'])
+def delete_task(post_id=None):
+        if post_id and chk_post_number(post_id):
+            if post_id in taskDatabase:
+#                 print(type(post_id))
+#                 print(post_id)        
+                task_hash = compute_hash(post_id)    
+                del id_to_uuid[task_hash]
+                del taskDatabase[post_id]        
+                return f"task with ID {post_id} was deleted"
+            else:
+                return f"task with ID {post_id} does not exist..."
+        else:
+            return f"you have to provide correct post ID (integer number) to remove..."
 
 @app.route('/tasks', methods=['POST'])
 def add_task():
@@ -60,8 +77,8 @@ def add_task():
         data = request.get_json()
         if data:
             compute_next_id()
-            print(type(data))
-            print(data)
+#             print(type(data))
+#             print(data)
             taskDatabase[next_id] = dict(data)
             new_task_hash = compute_hash(next_id)
             id_to_uuid[new_task_hash] = next_id
@@ -70,4 +87,4 @@ def add_task():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
